b4534345d8c528e1d9a4c03bbdc61cd1478753b1
jasleen <jasleen@jasleen-Inspiron-5521>
2014-10-10 19:29:19 -0700

    ser

--- server.js
+++ server.js
@@ -1,9 +1,23 @@
 var express = require('express');
 var app = express();
-
+var mysql = require('mysql');
+var db=require('./shared/config/opsworks');
+var connection=(mysql.createConnection(db.db));
 app.get('/', function(req, res) {
    res.json({message:"Hello Worlds"});
+   connection.query('CREATE TABLE if not exists `transactions`  ( 		  `id` int(11) DEFAULT NULL,		  `items` text,		  `total` varchar(45) DEFAULT NULL,		  `creditcard` varchar(45) DEFAULT NULL,		  `tranDate` timestamp NULL DEFAULT CURRENT_TIMESTAMP		) ',
+			function(err,rows)     {
+	    
+	    if(err)
+	       res.send("Error Selecting : %s ",err );
+	 
+	    res.send('customers',{page_title:"Customers - Node.js",data:rows});
+	                       
+	     });
 });
+
+
+   
 
 app.use(express.static('public'));
 
