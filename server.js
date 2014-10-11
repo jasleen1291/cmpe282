e17906ce8054dbceb9b652c988ff1064a66f2bd5
jasleen <jasleen@jasleen-Inspiron-5521>
2014-10-10 19:38:11 -0700

    f

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var mysql = require('mysql');
 var db=require('./shared/config/opsworks');
 var connection=(mysql.createConnection(db.db));
 app.get('/', function(req, res) {
-   res.json({message:"Hello Worlds"});
+  // res.json({message:"Hello Worlds"});
    connection.query('CREATE TABLE if not exists `transactions`  ( 		  `id` int(11) DEFAULT NULL,		  `items` text,		  `total` varchar(45) DEFAULT NULL,		  `creditcard` varchar(45) DEFAULT NULL,		  `tranDate` timestamp NULL DEFAULT CURRENT_TIMESTAMP		) ',
 			function(err,rows)     {
 	    
