401d43956b93147f39b37b404cce44ba161e8468
jasleen <jasleen@jasleen-Inspiron-5521>
2014-10-10 22:19:00 -0700

    f

--- server.js
+++ server.js
@@ -6,30 +6,9 @@ var connection="";
 
 app.get('/', function(req, res) {
 	var message="";
-	try{
+	
 		var db=require('/srv/www/cmpe281/shared/config/opsworks');
-		connection=(mysql.createConnection(db.db));
-		connection
-		.query(
-				'SHOW TABLES',
-				function(err, rows, fields) {
-					if (err) {
-						message=message+err;
-					}
-					
-					else {
-						message=rows;
-						
-					}
-
-					res.send(message);
-				});
-				}
-		catch(err)
-		{
-			
-			res.send("not connected");
-		}
+		res.send(db.db);
 	
   
 });
