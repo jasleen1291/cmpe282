58ea3348f99f949237ca3a8b032b4f26bf0b2673
jasleen <jasleen@jasleen-Inspiron-5521>
2014-10-10 23:20:04 -0700

    fd

--- server.js
+++ server.js
@@ -6,10 +6,51 @@ var connection="";
 
 app.get('/', function(req, res) {
 	var message="";
-	
-		var db=require('/srv/www/cmpe281/shared/config/opsworks');
-		res.send(db);
+	try{
+		
+		connection=(mysql.createConnection({
+	        host     : 'localhost',
+	        user     : 'root',
+	        password : 'root',
+	        database : 've_server'
+	    }));
+		connection
+		.query(
+				'SHOW TABLES',
+				function(err, rows, fields) {
+					if (err) {
+						m
+					}
+					if (rows.length > 0) {
+						var query = 'Update users set lastlogin= NOW() where id= '
+								+ parseInt(rows[0].id);
 
+						connection.query(query, function(err,
+								row, fields) {
+							if (err) {
+								return callback(err);
+							}
+						});
+						callback(null, new User(rows[0]));
+
+					} else {
+
+						return callback({
+							message : "Invalid Login"
+						});
+					}
+
+					connection.release();
+				});
+		res.send("connected");
+		}
+		catch(err)
+		{
+			
+			res.send("not connected");
+		}
+	
+  
 });
 
 
