04f991bb26507679e49ce9d17b684134ece9be54
jasleen <jasleen@jasleen-Inspiron-5521>
2014-10-11 18:18:10 -0700

    sd

--- server.js
+++ server.js
@@ -13,26 +13,14 @@ app.get('/', function(req, res) {
 	var db=require('/srv/www/cmpe281/shared/config/opsworks');
 	var connection = mysql.createConnection(db.db);
 	connection.connect();
-	var queryString = "CREATE DATABASE  IF NOT EXISTS `ve_server`; USE `ve_server`;"+ 
+	var queryString =  
 "CREATE TABLE IF NOT EXISTS `transactions` ("+
 "`id` int(11) DEFAULT NULL,"+
   "`items` text,"+
   "`total` varchar(45) DEFAULT NULL,"+
   "`creditcard` varchar(45) DEFAULT NULL,"+
   "`tranDate` timestamp NULL DEFAULT CURRENT_TIMESTAMP"+
-") ENGINE=InnoDB DEFAULT CHARSET=latin1;"+
-
-
-"CREATE TABLE IF NOT EXISTS `users` ("+
- " `id` int(11) NOT NULL AUTO_INCREMENT,"+
-  "`username` varchar(100) DEFAULT NULL,"+
-  "`password` varchar(45) DEFAULT NULL,"+
-  "`usertype` int(11) DEFAULT '0',"+
-  "`lastlogin` timestamp NULL DEFAULT '1970-01-01 08:00:01',"+
-  "PRIMARY KEY (`id`)"+
-") ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=latin1;"+
-";"
-	 
+") ENGINE=InnoDB DEFAULT CHARSET=latin1;";
 	connection.query(queryString, function(err, rows) {
 	    if (err) throw err;
 	 
@@ -48,5 +36,5 @@ app.get('/', function(req, res) {
 
 app.use(express.static('public'));
 
-app.listen(80);
+app.listen(3000);
 console.log('Listening on port 80');
