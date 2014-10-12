ff3c077ea9f586b30fe3b505c13312c7b2e96cef
jasleen <jasleen@jasleen-Inspiron-5521>
2014-10-12 02:22:22 -0700

    xc

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(bodyParser.urlencoded({
 var mysql =	require('mysql');
 var db=require('/srv/www/cmpe281/shared/config/opsworks');
 db.db.multipleStatements=true;
-var connectionpool = mysql.createPool(db);
+var connectionpool = mysql.createPool(db.db);
 
 app.get('/', function(req, res) {
 	connectionpool
