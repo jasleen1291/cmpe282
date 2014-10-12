3c0695e95e8d3df9204e56fa4552b8cc00e087c4
jasleen <jasleen@jasleen-Inspiron-5521>
2014-10-11 18:04:16 -0700

    Dump

--- server.js
+++ server.js
@@ -9,27 +9,37 @@ var db = new AWS.DynamoDB();
 
 
 app.get('/', function(req, res) {
-	var message="";
-	/*try{
-		var db=require('/srv/www/cmpe281/shared/config/opsworks');
-		res.json({db:db,
-		key:process.env.AWSSecretKey,
-		id:process.env.AWSAccessKeyId
-		}
-				
-		);
-		}
-		catch(err)
-		{
-			
-		}
-	*/
-	db.listTables(function(err, data) {
-		if(err)
-			res.json(err);
-		else
-		res.json(data.TableNames);
-		});
+	var mysql =	require('mysql');
+	var db=require('/srv/www/cmpe281/shared/config/opsworks');
+	var connection = mysql.createConnection(db.db);
+	connection.connect();
+	var queryString = "CREATE DATABASE  IF NOT EXISTS `ve_server` USE `ve_server`;"+ 
+"CREATE TABLE `transactions` ("+
+"`id` int(11) DEFAULT NULL,"+
+  "`items` text,"+
+  "`total` varchar(45) DEFAULT NULL,"+
+  "`creditcard` varchar(45) DEFAULT NULL,"+
+  "`tranDate` timestamp NULL DEFAULT CURRENT_TIMESTAMP"+
+") ENGINE=InnoDB DEFAULT CHARSET=latin1;"+
+
+
+"CREATE TABLE `users` ("+
+ " `id` int(11) NOT NULL AUTO_INCREMENT,"+
+  "`username` varchar(100) DEFAULT NULL,"+
+  "`password` varchar(45) DEFAULT NULL,"+
+  "`usertype` int(11) DEFAULT '0',"+
+  "`lastlogin` timestamp NULL DEFAULT '1970-01-01 08:00:01',"+
+  "PRIMARY KEY (`id`)"+
+") ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=latin1;"+
+";"
+	 
+	connection.query(queryString, function(err, rows) {
+	    if (err) throw err;
+	 
+	    res.send(rows);
+	});
+	 
+	connection.end();
   
 });
 
