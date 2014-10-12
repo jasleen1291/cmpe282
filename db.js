920366d4cb688e20b91e0caf145404fa12d212a9
jasleen <jasleen@jasleen-Inspiron-5521>
2014-10-12 01:42:14 -0700

    dsas

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 var mysql =	require('mysql');
 var db=require('/srv/www/cmpe281/shared/config/opsworks');
-db.multipleStatements=true;
+db.db.multipleStatements=true;
 var connectionpool = mysql.createPool(db);
 module.exports =  connectionpool;
 
